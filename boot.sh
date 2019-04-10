7a9cd1c53f704175d67b0d20b455c0e8d6e7e206
Serhii Senyk <sergiysenyk18@gmail.com>
2019-04-10 17:36:45 +0300

    boot

--- boot.sh
+++ boot.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 source venv/bin/activate
-#flask db upgrade
-flask  compile
-exec gunicorn -b :5000 --access-logfile - --error-logfile - pages:app
+flask db upgrade
+flask  translate compile
+gunicorn -b :5000 --access-logfile - --error-logfile - pages:app
